6123d94896fafd6c1f1ccdc97b5476e1b07895e6
Dhananjay Singh <dhananjay1204@gmail.com>
2018-03-04 16:21:10 +0530

    integration-test starter

--- writing-test/src/app/app.module.ts
+++ writing-test/src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { TodosComponent } from './2-todos/todos.component';
+import { UserDetailsComponent } from './3-user-details/user-details.component';
+import { VoterComponent } from './1-voter/voter.component';
+import { UsersComponent } from './users/users.component';
+import { RouterModule } from '@angular/router';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { routes } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TodosComponent,
+    UserDetailsComponent,
+    VoterComponent,
+    UsersComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
