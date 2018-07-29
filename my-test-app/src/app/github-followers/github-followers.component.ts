bc0a0504b4a85cadf464fcec7ae4fa50f25b1598
Dhananjay Singh <dhananjay1204@gmail.com>
2018-07-30 04:54:52 +0530

    upgraded to 6.1.1 test-app

--- my-test-app/src/app/github-followers/github-followers.component.ts
+++ my-test-app/src/app/github-followers/github-followers.component.ts
@@ -1,10 +1,13 @@
+
+import {combineLatest as observableCombineLatest,  Observable } from 'rxjs';
+
+import {switchMap} from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
 import { GithubFollowersService } from './../services/github-followers.service';
 import { Component, OnInit } from '@angular/core';
-import 'rxjs/add/observable/combineLatest';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/switchMap';
+
+
+
 
 @Component({
   selector: 'github-followers',
@@ -18,16 +21,16 @@ export class GithubFollowersComponent implements OnInit {
 
   ngOnInit() {
 
-    Observable.combineLatest([
+    observableCombineLatest([
       this.route.paramMap,
       this.route.queryParamMap
-    ])
-      .switchMap(combine => {
+    ]).pipe(
+      switchMap(combine => {
         let id = combine[0].get('id');
         let page = combine[1].get('page');
         console.log(page);
         return this.service.getAll();
-      })
+      }))
       .subscribe(followers => {
         this.followers = followers;
       })
--- my-test-app/src/app/services/data.service.ts
+++ my-test-app/src/app/services/data.service.ts
@@ -1,12 +1,15 @@
+
+import {throwError as observableThrowError,  Observable } from 'rxjs';
+
+import {catchError, map} from 'rxjs/operators';
 import { BadInput } from './../common/bad-input';
 import { NotFoundError } from './../common/not-found-error';
 import { AppError } from './../common/app.error';
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/observable/throw';
+
+
+
 
 @Injectable()
 export class DataService {
@@ -15,37 +18,37 @@ export class DataService {
     constructor(private url: string, private http: Http) { }
 
     getAll() {
-        return this.http.get(this.url)
-            .map(response => response.json())
-            .catch(this.handleError);
+        return this.http.get(this.url).pipe(
+            map(response => response.json()),
+            catchError(this.handleError),);
     }
 
     create(resource) {
-        return this.http.post(this.url, JSON.stringify(resource))
-            .map(response => response.json())
-            .catch(this.handleError);
+        return this.http.post(this.url, JSON.stringify(resource)).pipe(
+            map(response => response.json()),
+            catchError(this.handleError),);
     }
 
     update(resource) {
-        return this.http.patch(this.url + '/' + resource.id, JSON.stringify({ isRead: true }))
-            .map(response => response.json())
-            .catch(this.handleError);
+        return this.http.patch(this.url + '/' + resource.id, JSON.stringify({ isRead: true })).pipe(
+            map(response => response.json()),
+            catchError(this.handleError),);
     }
 
     delete(id) {
-        return this.http.delete(this.url + '/' + id)
-            .map(response => response.json())
-            .catch(this.handleError);
+        return this.http.delete(this.url + '/' + id).pipe(
+            map(response => response.json()),
+            catchError(this.handleError),);
     }
 
     private handleError(error: Response) {
         if (error.status === 400) {
-            return Observable.throw(new BadInput(error.json()));
+            return observableThrowError(new BadInput(error.json()));
         }
         if (error.status === 404) {
-            return Observable.throw(new NotFoundError(error));
+            return observableThrowError(new NotFoundError(error));
         }
-        return Observable.throw(new AppError(error));
+        return observableThrowError(new AppError(error));
     }
 
 }
