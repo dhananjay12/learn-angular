ff6f55c284875a7728623b63b81e2f21db7c0bee
dhananjay12 <dhananjay1204@gmail.com>
2020-12-29 13:43:19 +0100

    Change

--- test-versioning/src/app/app.component.ts
+++ test-versioning/src/app/app.component.ts
@@ -7,6 +7,6 @@ import {environment} from '../environments/environment';
     <h3>v{{currentApplicationVersion}}</h3>`
 })
 export class AppComponent {
-  title = 'Demo Application for version numbering';
+  title = 'Demo Application for Version numbering';
     currentApplicationVersion = environment.appVersion;
 }
